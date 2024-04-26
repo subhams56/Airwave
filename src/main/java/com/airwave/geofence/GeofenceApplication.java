3c4fa896e76d7a8978537d6c6664b553f45b719f
subham <subhams56@gmail.com>
2024-04-26 09:26:22 +0530

    intiial commit

---
+++ src/main/java/com/airwave/geofence/GeofenceApplication.java
@@ -0,0 +1,13 @@
+package com.airwave.geofence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeofenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeofenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airwave/geofence/GeofenceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.airwave.geofence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeofenceApplicationTests {
+
+}
