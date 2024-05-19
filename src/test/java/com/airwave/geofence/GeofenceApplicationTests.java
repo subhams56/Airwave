b9b49f98d15816851102a4ad281e21deea62eef8
subham <subhams56@gmail.com>
2024-05-19 12:48:41 +0530

    test class vulenarbility fix 1

--- src/test/java/com/airwave/geofence/GeofenceApplicationTests.java
+++ src/test/java/com/airwave/geofence/GeofenceApplicationTests.java
@@ -3,7 +3,15 @@ package com.airwave.geofence;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class GeofenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Simple test to check that the context loads successfully
+        assertTrue(true);
+    }
 
 }
